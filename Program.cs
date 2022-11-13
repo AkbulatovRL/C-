45435a3b7407aa19486ce78dc0aba2c3d365359e
Akbulatov Ruslan <72865134+AkbulatovRL@users.noreply.github.com>
2022-11-13 23:59:04 +0300

    numbers max

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int a, b;
+Console.WriteLine("num a");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("num b");
+b = Convert.ToInt32(Console.ReadLine());
+
+if(a>b){
+    Console.WriteLine(a);
+}
+
+else{
+    Console.WriteLine(b);
+}
